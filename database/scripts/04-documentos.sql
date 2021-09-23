44a2bc34fde2bdb79ad71741513cdb8c0d386568
Renzo Diaz <renzodd2001@gmail.com>
2021-09-22 19:04:33 -0500

    Scripts DB

---
+++ database/scripts/04-documentos.sql
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2021 (c) Renzo Diaz
+ * 
+ * Tabla de documentos
+ * URL: http://localhost/phpmyadmin/
+ */
+ 
+DELIMITER //
+
+DROP TABLE IF EXISTS Documentos //
+CREATE TABLE Documentos (
+	DocumentoID			INTEGER			AUTO_INCREMENT,
+	
+    EmpresaID			INTEGER			NOT NULL,
+	TipoDocumento		TEXT			NOT NULL,  -- LETRA / FACTURA / RECIBO
+	Emisor				TEXT			NOT NULL,
+	FechaEmision		DATE			NOT NULL,
+	FechaVencimiento	DATE			NOT NULL,	
+	Moneda				TEXT			NOT NULL,  -- PEN / USD
+	Monto				DECIMAL(19,9)	NOT NULL,
+	EnFinanciamiento	DECIMAL(19,9)	NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (DocumentoID),
+	FOREIGN KEY (EmpresaID)	REFERENCES EMPRESAS (EmpresaID)
+) //
+
+DROP PROCEDURE IF EXISTS DOCUMENTOS_CREAR //
+CREATE PROCEDURE DOCUMENTOS_CREAR (IN EmpresaID INTEGER, IN TipoDocumento TEXT, IN Emisor TEXT, IN FechaEmision DATE, IN FechaVencimiento DATE, IN Moneda TEXT, IN Monto DECIMAL(19,9))
+BEGIN
+	INSERT INTO Documentos (EmpresaID, TipoDocumento, Emisor, FechaEmision, FechaVencimiento, Moneda, Monto)
+	VALUES 	(EmpresaID, TipoDocumento, Emisor, FechaEmision, FechaVencimiento, Moneda, Monto);
+
+	SELECT 	D.*
+	FROM 	Documentos AS D
+	WHERE 	D.EmpresaID = EmpresaID
+			AND D.TipoDocumento = TipoDocumento
+			AND D.Emisor = Emisor
+			AND D.FechaEmision = FechaEmision
+			AND D.FechaVencimiento = FechaVencimiento
+			AND D.Moneda = Moneda
+			AND D.Monto = Monto;
+END //
+
+DROP PROCEDURE IF EXISTS DOCUMENTOS_LEER_EMPRESA //
+CREATE PROCEDURE DOCUMENTOS_LEER_EMPRESA (IN EmpresaID INTEGER)
+BEGIN
+	SELECT 	D.*
+	FROM 	Documentos AS D
+	WHERE	D.EmpresaID = EmpresaID
+	ORDER BY D.FechaEmision;
+END //
+
+DROP PROCEDURE IF EXISTS DOCUMENTOS_LEER_EMPRESA_MONEDA //
+CREATE PROCEDURE DOCUMENTOS_LEER_EMPRESA_MONEDA (IN EmpresaID INTEGER, IN Moneda TEXT)
+BEGIN
+	SELECT 	D.*
+	FROM 	Documentos AS D
+	WHERE	D.EmpresaID = EmpresaID
+			AND D.Moneda = Moneda
+	ORDER BY D.FechaEmision;
+END //
+
+DROP PROCEDURE IF EXISTS DOCUMENTOS_LEER_FINANCIAMIENTO_DISPONIBLE //
+CREATE PROCEDURE DOCUMENTOS_LEER_FINANCIAMIENTO_DISPONIBLE (IN EmpresaID INTEGER, IN FechaActual DATE)
+BEGIN
+	SELECT 	D.*
+	FROM 	Documentos AS D
+	WHERE	D.EmpresaID = EmpresaID
+			AND D.EnFinanciamiento < D.Monto
+			AND D.FechaVencimiento > FechaActual
+	ORDER BY D.FechaEmision;
+END //
+
+DROP PROCEDURE IF EXISTS DOCUMENTOS_LEER_CON_FINANCIAMIENTO //
+CREATE PROCEDURE DOCUMENTOS_LEER_CON_FINANCIAMIENTO (IN EmpresaID INTEGER, IN FechaActual DATE)
+BEGIN
+	SELECT 	D.*
+	FROM 	Documentos AS D
+	WHERE	D.EmpresaID = EmpresaID
+			AND D.EnFinanciamiento > 0
+	ORDER BY D.FechaEmision;
+END //
